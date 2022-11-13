52a76fc30fac7721eaaf39010b1c7707332d46b6
ryushooter <danielmartinez411@gmail.com>
2022-11-13 12:29:57 -0500

    Inicio del proyecto

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDteuE2xPz9iFL8zkytaC_QtJ6ffborxW8",
+  authDomain: "react-fb-15910.firebaseapp.com",
+  projectId: "react-fb-15910",
+  storageBucket: "react-fb-15910.appspot.com",
+  messagingSenderId: "546679848239",
+  appId: "1:546679848239:web:5a205452f24fa7ca7635da"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
